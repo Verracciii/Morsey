4c382aa283d4f575172795db4126afae50c9a04d
Verracciii <valterssocial@gmail.com>
2025-03-13 09:54:56 +0000

    Add TouchInterrupt behavior and .gitignore for behaviors directory
    - Create `TouchInterrupt.java` in `src/behaviors`
    - Implement `Behavior` interface in `TouchInterrupt`
    - Add placeholder methods in `TouchInterrupt`
    - Add main method in `TouchInterrupt`
    - Add `.gitignore` file in `bin` directory
    - Ignore `behaviors` directory in `.gitignore`

---
+++ src/behaviors/TouchInterrupt.java
@@ -0,0 +1,33 @@
+package behaviors;
+
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.subsumption.Behavior;
+
+public class TouchInterrupt implements Behavior {
+
+	EV3TouchSensor TOUCH_SEN;
+	
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
