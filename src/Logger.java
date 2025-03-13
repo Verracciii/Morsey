90284a94eaaf51b656d2d954f57021ea1e8163c8
Verracciii <valterssocial@gmail.com>
2025-03-13 10:28:46 +0000

    Add Logger and Morsey classes, update MorseReaderBase with Morse code map
    - Add Logger class extending Thread
    - Add Morsey class with main method
    - Update MorseReaderBase with Morse code map
    - Implement Morse code decoding in MorseReaderBase
    - Update .gitignore to include Logger.class and Morsey.class

---
+++ src/Logger.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+public class Logger extends Thread {
+
+	private Map<String, int[]> ITEM_COORDS;
+	
+	public void run() { }
+	
+	private void updateUSData() { }
+	
+	private void updateColorData() { }
+	
+	private void updateMorse() { }
+	
+	private void updateMorseWord() { }
+	
+
+	
+}
