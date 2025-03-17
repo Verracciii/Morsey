ada42a5e38533d44fbd94d8334d529ce723e2b16
Gagan <gghatahora1@gmail.com>
2025-03-17 18:38:40 +0000

    Feature #12: Created Touch Interrupt class

--- src/behaviors/TouchInterrupt.java
+++ src/behaviors/TouchInterrupt.java
@@ -1,29 +1,39 @@
 package behaviors;
 
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.port.SensorPort;
 import lejos.robotics.subsumption.Behavior;
+import lejos.robotics.SampleProvider;
+import morse.MotorController;
 
 public class TouchInterrupt implements Behavior {
 
-	private EV3TouchSensor TOUCH_SEN;
-	
-	@Override
-	public boolean takeControl() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    private EV3TouchSensor touchSensor;
+    private MotorController motorController;
+    private SampleProvider touchSampleProvider;
+    private float[] sample;
 
-	@Override
-	public void action() {
-		// TODO Auto-generated method stub
+    public TouchInterrupt(MotorController motorController) {
+        this.touchSensor = new EV3TouchSensor(SensorPort.S1);
+        this.touchSampleProvider = touchSensor.getTouchMode();
+        this.sample = new float[touchSampleProvider.sampleSize()];
+        this.motorController = motorController;
+    }
 
-	}
+    @Override
+    public boolean takeControl() {
+        touchSampleProvider.fetchSample(sample, 0);
+        return sample[0] == 1; // 1 means pressed, 0 means not pressed
+    }
 
-	@Override
-	public void suppress() {
-		// TODO Auto-generated method stub
+    @Override
+    public void action() {
+        System.out.println("Touch sensor pressed! Stopping robot.");
+        motorController.stop(); // Stop the motors
+    }
 
-	}
-
-
+    @Override
+    public void suppress() {
+        // No suppression needed since this behavior stops the robot
+    }
 }
--- src/morse/TouchMorseReader.java
+++ src/morse/TouchMorseReader.java
@@ -1,8 +1,12 @@
 package morse;
 
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
 import lejos.utility.Delay;
+import behaviors.TouchInterrupt;
 
 public class TouchMorseReader extends Thread implements MorseReader {
 
@@ -14,7 +18,27 @@ public class TouchMorseReader extends Thread implements MorseReader {
     public StringBuilder WORD;
     public StringBuilder MORSE;
     private boolean running;
+    private MotorController motorController; // MotorController for motor control
+    private Arbitrator arbitrator; // Arbitrator to manage behaviors
 
+    public TouchMorseReader() {
+        // Initialize the touch sensor
+        this.TOUCH_SEN = new EV3TouchSensor(SensorPort.S1);
+
+        // Initialize the MotorController
+        this.motorController = new MotorController();
+
+        // Create the TouchInterrupt behavior
+        Behavior touchInterrupt = new TouchInterrupt(motorController);
+
+        // Create an array of behaviors for the arbitrator
+        Behavior[] behaviors = { touchInterrupt };
+
+        // Initialize the arbitrator with the behaviors
+        this.arbitrator = new Arbitrator(behaviors);
+    }
+
+    
     @Override
     public void run() {
         // Implement the logic for touch reader mode
@@ -23,6 +47,9 @@ public class TouchMorseReader extends Thread implements MorseReader {
         LCD.drawString("Touch Mode", 0, 0);
         // Add your logic here to read Morse code using the touch sensor
         Delay.msDelay(1000);
+        
+        // Start the arbitrator to manage behaviors
+        arbitrator.go();
     }
 
     @Override
